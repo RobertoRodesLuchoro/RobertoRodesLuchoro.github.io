10e640dbacced214be3a81c87f35e8092fc7e622
Rob <robertorodesluchorodev@gmail.com>
2025-06-23 01:13:03 +0200

    new page uploaded

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        'spin-border': {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        'spin-border': 'spin-border 2s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+
