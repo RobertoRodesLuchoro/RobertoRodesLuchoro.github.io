5870e3be3559a86c626793a56acf982d0530de8f
Rob <robertorodesluchorodev@gmail.com>
2025-06-23 01:24:36 +0200

    trying to upload the page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'static',
+  site: 'https://robertorodesluchoro.github.io/',
+  base: '/robertorodesluchoro.github.io/'
 });
